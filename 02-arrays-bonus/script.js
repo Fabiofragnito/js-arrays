713cfba385d8c29057e71068cda3103582615a50
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-02 17:11:23 +0200

    editin 2nd one

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,7 +16,13 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+
+// const longNames = null;
+// teachers.length(5)
+// 
+
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
 let index = teachers.indexOf("Ed");
